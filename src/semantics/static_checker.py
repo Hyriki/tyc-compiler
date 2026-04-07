b449430b781444c0a459a0bf374c06ccf388cc3c
Hyriki <realhyriki@gmail.com>
2026-04-07 23:18:30 +0700

    update

--- src/semantics/static_checker.py
+++ src/semantics/static_checker.py
@@ -8,6 +8,7 @@ specified in the TyC language specification.
 """
 
 from functools import reduce
+from platform import node
 from typing import (
     Dict,
     List,
@@ -78,16 +79,44 @@ from .static_error import (
 
 class StaticChecker(ASTVisitor):
     def visit_program(self, node: "Program", o: Any = None):
-        pass
+        structSeen = {}
+        funcSeen = {}
+        for decl in node.decls:
+            if isinstance(decl, StructDecl):
+                if decl.name in structSeen:
+                    raise Redeclared("Struct", decl.name)
+                structSeen[decl.name] = decl
+            elif isinstance(decl, FuncDecl):
+                if decl.name in funcSeen:
+                    raise Redeclared("Function", decl.name)
+                funcSeen[decl.name] = decl
+        dictToPass = {"structs": structSeen, "functions": funcSeen, "vars": [{}]}
+        for decl in node.decls:
+            self.visit(decl, dictToPass)
+        
 
     def visit_struct_decl(self, node: "StructDecl", o: Any = None):
-        pass
+        seen = {}
+        for member in node.members:
+            if member.name in seen:
+                raise Redeclared("Variable", member.name)
+            seen[member.name] = member
+            self.visit(member, o)
 
     def visit_member_decl(self, node: "MemberDecl", o: Any = None):
         pass
 
     def visit_func_decl(self, node: "FuncDecl", o: Any = None):
-        pass
+        paramSeen = {}
+        for param in node.params:
+            if param.name in paramSeen:
+                raise Redeclared("Parameter", param.name)
+            paramSeen[param.name] = param
+            self.visit(param, o)
+
+        self.visit(node.return_type, o)
+        new_dict = {**o, "vars": o["vars"] + [paramSeen]}
+        self.visit(node.body, new_dict)
 
     def visit_param(self, node: "Param", o: Any = None):
         pass
@@ -110,19 +139,39 @@ class StaticChecker(ASTVisitor):
 
     # Statements
     def visit_block_stmt(self, node: "BlockStmt", o: Any = None):
-        pass
+        localVars = {}
+        new_dict = {**o, "vars": o["vars"] + [localVars]}
+        for stmt in node.statements:
+            self.visit(stmt, new_dict) #Avoid int x = x + 1
+            if isinstance(stmt, VarDecl):
+                if stmt.name in localVars:
+                    raise Redeclared("Variable", stmt.name)
+                localVars[stmt.name] = stmt
 
     def visit_var_decl(self, node: "VarDecl", o: Any = None):
-        pass
+        if node.init_value:
+            self.visit(node.init_value, o)
+
 
     def visit_if_stmt(self, node: "IfStmt", o: Any = None):
-        pass
+        self.visit(node.condition, o)
+        self.visit(node.then_body, o)
+        if node.else_body:
+            self.visit(node.else_body, o)
 
     def visit_while_stmt(self, node: "WhileStmt", o: Any = None):
-        pass
+        self.visit(node.condition, o)
+        self.visit(node.body, o)
 
     def visit_for_stmt(self, node: "ForStmt", o: Any = None):
-        pass
+        if node.init:
+            self.visit(node.init, o)
+        if node.condition:
+            self.visit(node.condition, o)
+        if node.update:
+            self.visit(node.update, o)
+        self.visit(node.body, o)
+
 
     def visit_switch_stmt(self, node: "SwitchStmt", o: Any = None):
         pass
@@ -140,23 +189,29 @@ class StaticChecker(ASTVisitor):
         pass
 
     def visit_return_stmt(self, node: "ReturnStmt", o: Any = None):
-        pass
+        if node.expr:
+            self.visit(node.expr, o)
+
 
     def visit_expr_stmt(self, node: "ExprStmt", o: Any = None):
-        pass
+        self.visit(node.expr, o)
 
     # Expressions
     def visit_binary_op(self, node: "BinaryOp", o: Any = None):
-        pass
+        self.visit(node.left, o)
+        self.visit(node.right, o)
+
 
     def visit_prefix_op(self, node: "PrefixOp", o: Any = None):
-        pass
+        self.visit(node.operand, o)
 
     def visit_postfix_op(self, node: "PostfixOp", o: Any = None):
-        pass
+        self.visit(node.operand, o)
 
     def visit_assign_expr(self, node: "AssignExpr", o: Any = None):
-        pass
+        self.visit(node.left, o)
+        self.visit(node.right, o)
+
 
     def visit_member_access(self, node: "MemberAccess", o: Any = None):
         pass
@@ -165,7 +220,12 @@ class StaticChecker(ASTVisitor):
         pass
 
     def visit_identifier(self, node: "Identifier", o: Any = None):
-        pass
+        for scope in reversed(o["vars"]):
+            if node.name in scope:
+                break
+        else:
+            raise UndeclaredIdentifier(node.name)
+
 
     def visit_struct_literal(self, node: "StructLiteral", o: Any = None):
         pass
